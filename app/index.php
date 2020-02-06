9f2771640b8b0d5e3bec5e5647d3b07e1db1c9e2
krisdarryn <krisdarryn3@gmail.com>
2020-02-06 18:47:36 +0800

    Create mysql docker container.

--- app/index.php
+++ app/index.php
@@ -1,3 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Docker PHP - Local Development</title>
+    </head>
+    <body>
+        <center>
+            <h1>Put your source code in <cite>/app</cite> directory.</h1>
+        </center>
 
-phpinfo();
+        <?php phpinfo(); ?>
+    </body>
+</html>
